e46bd37b82d6eedf43962fa5819a463d31996c49
energyinpython <92691295+energyinpython@users.noreply.github.com>
2021-11-23 21:56:02 +0100

    Update radar_charts.py

--- radar_charts/radar_charts.py
+++ radar_charts/radar_charts.py
@@ -4,44 +4,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-#https://www.kaggle.com/typewind/draw-a-radar-chart-with-python-in-a-simple-way
-# https://www.codegrepper.com/code-examples/python/python+radar+chart
-met = 'topsis'
-dane = pd.read_csv(met + '.csv', index_col='Ai')
-
-# first approach
-fig=plt.figure()
-ax = fig.add_subplot(111, polar=True)
-for ind in list(dane.index):
-    # set labels - points on the perimeter of the chart
-    # labels - years
-    labels=np.array(list(dane.columns))
-    # alternatives
-    stats=dane.loc[ind, labels].values
-
-    angles=np.linspace(0, 2*np.pi, len(labels), endpoint=False)
-    # close the plot
-    stats=np.concatenate((stats,[stats[0]]))
-    angles=np.concatenate((angles,[angles[0]]))
-    
-    lista = list(dane.columns)
-    lista.append(dane.columns[0])
-    labels=np.array(lista)
-
-    ax.plot(angles, stats, 'o-', linewidth=2)
-    #ax.fill(angles, stats, alpha=0.25)
-ax.set_thetagrids(angles * 180/np.pi, labels)
-ax.grid(True)
-#plt.legend(dane.index, loc = 'right')
-plt.legend(dane.index, bbox_to_anchor=(1.1, 0.95, 0.3, 0.2), loc='upper left')
-plt.show()
-
-
-
-# second approach
-#labels were columns (years) but now we want alternatives
-#linie to byly alternatives a chcemy metody czyli labele - kolumny - dane.columns
-#as labels we want dane.index
+#labels are alternatives (dane.index)
+#lines are methods (dane.columns)
 
 methods = ['topsis',
            'vikor',
