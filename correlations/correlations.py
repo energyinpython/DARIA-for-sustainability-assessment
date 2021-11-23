6a1ca16ee708d6a3c8d1b9cb641a7b7e080a51ef
energyinpython <92691295+energyinpython@users.noreply.github.com>
2021-11-23 21:48:47 +0100

    Update correlations.py

--- correlations/correlations.py
+++ correlations/correlations.py
@@ -1,37 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-#from itertools import product, combinations
 import seaborn as sns
-#from pandas.plotting import scatter_matrix
-#import os
 import copy
-#import matplotlib
-#import matplotlib.gridspec as gridspec
 from scipy.stats import pearsonr
 
 
-# weighted Spearman rW
-def weighted_spearman(R, Q):
-    N = len(R)
-    denom = N**4 + N**3 - N**2 - N
-    reszta = (N-R+1)+(N-Q+1)
-    suma = 6*sum((R-Q)**2*reszta)
-    rW = 1-(suma/denom)
-    return rW
-
-# WS Similarity coefficient
-def coeff_WS(R, Q):
-    sWS = 0
-    N = len(R)
-    for i in range(N):
-        sWS += 2**(-int(R[i]))*(abs(R[i]-Q[i])/max(abs(R[i] - 1), abs(R[i] - N)))
-    WS = 1 - sWS
-    return WS
-
-
 def draw_heatmap(df_new_heatmap, title):
-    #plt.figure(figsize = (8,5))
     sns.set(font_scale=1.4)
     heatmap = sns.heatmap(df_new_heatmap, annot=True, fmt=".4f", cmap="BuPu",
                           linewidth=0.5, linecolor='w')
@@ -47,8 +22,6 @@ years = ['2015',
          '2019',
          ]
 
-#years = ['2015']
-
 for year in years:
     data = pd.read_csv('results_rank_' + year + '.csv')
     data = data.iloc[:, 1:]
@@ -57,34 +30,17 @@ for year in years:
 
     method_types = list(data.columns)
 
-    dict_new_heatmap_rw = {'TOPSIS': [], 
+    dict_new_heatmap_pearson = {'TOPSIS': [], 
                             'VIKOR': [], 
                             'COMET': [],
                             }
 
-    #dict_new_heatmap_ws = copy.deepcopy(dict_new_heatmap_rw)
-
-    dict_new_heatmap_pearson = copy.deepcopy(dict_new_heatmap_rw)
-
-
     # heatmaps for correlations coefficients
     for i in method_types[::-1]:
         for j in method_types:
-            print('i: ', i, ' j: ', j)
-            #dict_new_heatmap_rw[j].append(weighted_spearman(data[i], data[j]))
-            #dict_new_heatmap_ws[j].append(coeff_WS(data[i], data[j]))
             corr_p, _ = pearsonr(data[i], data[j])
             dict_new_heatmap_pearson[j].append(corr_p)
         
-
-    '''
-    df_new_heatmap_rw = pd.DataFrame(dict_new_heatmap_rw, index = method_types[::-1])
-    df_new_heatmap_rw.columns = method_types
-
-    df_new_heatmap_ws = pd.DataFrame(dict_new_heatmap_ws, index = method_types[::-1])
-    df_new_heatmap_ws.columns = method_types
-    '''
-
     df_new_heatmap_pearson = pd.DataFrame(dict_new_heatmap_pearson, index = method_types[::-1])
     df_new_heatmap_pearson.columns = method_types
 
@@ -92,15 +48,3 @@ for year in years:
     draw_heatmap(df_new_heatmap_pearson, r'$Pearson$' + ', Year: ' + year)
     plt.savefig('pearson_' + str(year) + '.pdf')
     plt.show()
-
-    '''
-    # correlation matrix with rw coefficient
-    draw_heatmap(df_new_heatmap_rw, r'$r_w$')
-    plt.savefig('rw_' + str(year) + '.pdf')
-    plt.show()
-
-    # correlation matrix with WS coefficient
-    draw_heatmap(df_new_heatmap_ws, r'$WS$')
-    plt.savefig('ws_' + str(year) + '.pdf')
-    plt.show()
-    '''
